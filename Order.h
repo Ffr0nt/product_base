1636df5cb81421658cfd6d3ff14d5f9484036a86
Fedor <fedzakh02@ya.ru>
2022-06-05 16:22:23 +0300

    product.h

--- Order.h
+++ Order.h
@@ -4,5 +4,24 @@
 
 #ifndef PRODUCT_BASE_ORDER_H
 #define PRODUCT_BASE_ORDER_H
+#include "Position.h"
+#include "vector"
+
+class Order{
+public:
+    Order() = default;
+    void add_position(){};
+
+    double get_cost(Position* ptr_pos);
+
+    void get_info();
+
+    bool empty();
+
+private:
+    void clear();
+
+    std::vector<Position*> m_ptr_positions;
+};
 
 #endif //PRODUCT_BASE_ORDER_H
--- Position.h
+++ Position.h
@@ -10,20 +10,20 @@
 // get virtual pure methods get_cost, get_quantity
 class Position{
 public:
-    Position(Product product): m_product(&product){};
+    Position(Product* product): m_product(product){};
 
     Product* get_ptr_product(){return m_product;}
 
-    virtual double get_cost(){};
+    virtual double get_cost()=0;
 
-    virtual double get_quantity(){};
+    virtual double get_quantity()=0;
 protected:
     Product* m_product;
 };
 
 class AmountPosition: public Position{
 public:
-    AmountPosition(Product product, size_t amount): Position(product), m_amount(amount){};
+    AmountPosition(Product* product, size_t amount): Position(product), m_amount(amount){};
 
     double get_cost() override{ return m_product->get_price() * m_amount;}
 
@@ -31,6 +31,18 @@ public:
 
 private:
     size_t m_amount;
+};
+
+class WeightPosition: public Position{
+public:
+    WeightPosition(Product* product, size_t weight): Position(product), m_weight(weight){};
+
+    double get_cost() override{ return m_product->get_price() * m_weight;}
+
+    double get_quantity() override {return m_weight;}
+
+private:
+    size_t m_weight;
 };
 
 #endif //PRODUCT_BASE_POSITION_H
